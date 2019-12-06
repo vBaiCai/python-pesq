5e7979047458c9b3d39504d667696215add4addc
vBaiCai <radon86@live.cn>
2019-12-06 14:12:35 +0800

    fix import error in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 from setuptools import setup, Extension, find_packages
-import numpy as np
+from setuptools.command.build_ext import build_ext as _build_ext
 import os
 
 extension = Extension("pesq_core",
                       sources=["pypesq/pesq.c", "pypesq/dsp.c", "pypesq/pesqdsp.c", "pypesq/pesqio.c", "pypesq/pesqmain.c", "pypesq/pesqmod.c"],
-                      include_dirs=[os.path.join(np.get_include(), 'numpy')], 
+                      # include_dirs=[os.path.join(np.get_include(), 'numpy')], 
                       language='c++')
 
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
 setup(name='pypesq',
-    version='1.0',
+    version='1.1',
     description="A package to compute pesq score.",
     url='https://github.com/vBaiCai/python-pesq',
+    author_email='zhuroubaicai@gmail.com',
+    keywords=['pesq', 'speech', 'speech quality'],
     license='MIT',
     packages=find_packages(),
     ext_modules=[extension],
+    cmdclass={'build_ext': build_ext},
+    setup_requires=['numpy'],
     py_modules=['numpy'],
     zip_safe=False,
     install_requires=['numpy'],
